8a4be9b8484c77f54ce693921a19c29b3ca36dbd
Triston Burns <tburns@aurasolutionsllc.com>
2025-10-23 21:46:23 -0700

    Add auth provider, router provider, and router devtools to App component

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+import { createRootRoute, createRouter, Outlet, RouterProvider } from "@tanstack/react-router"
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools"
+import { AuthProvider, useAuth } from './auth';
 
+const rootRoute = createRootRoute({
+  component: () => (
+    <>
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  ),
+});
+
+const routeTree = rootRoute.addChildren([
+  // ... other routes
+]);
+
+const router = createRouter({
+  routeTree,
+});
+
+function InnerApp() {
+  const auth = useAuth()
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RouterProvider router={router} context={auth} />
+      <TanStackRouterDevtools router={router} />
     </>
-  )
+  );
+}
+
+function App() {
+  return (
+    <AuthProvider>
+      <InnerApp />
+    </AuthProvider>
+  );
 }
 
 export default App
